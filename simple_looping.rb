399f68763cca965caf45d732f71e0b6c0583828d
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-07 01:51:46 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,8 +12,11 @@ def loop_iterator(number_of_times)
   counter = 0 
   loop do
     counter += 1 
-    
-  
+    puts phrase
+    if counter >= number_of_times
+      break
+    end
+  end
 end
 
 def times_iterator(number_of_times)
